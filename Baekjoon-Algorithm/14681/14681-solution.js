69b98911c4ba60b13b2e53db8fa764b01aa6dad4
Hojin Jang <onewithtruth@gmail.com>
2021-10-05 22:48:19 +0900

    Update 14681-solution.js

--- Baekjoon-Algorithm/14681/14681-solution.js
+++ Baekjoon-Algorithm/14681/14681-solution.js
@@ -1,16 +1,17 @@
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 let input = [];
 
 rl.on("line", function (line) {
-  input.push(Number(line));
+  input.push(line);
+  //input = line.split(' ').map((el) => parseInt(el));
 }).on("close", function () {
-  const a = input[0];
-  const b = input[1];
+  const a = Number(input[0]);
+  const b = Number(input[1]);
 
   let output;
 
