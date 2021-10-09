46df849edaf72a227051b0a55392f1cf551987bc
Hojin Jang <onewithtruth@gmail.com>
2021-10-09 23:23:19 +0900

    Create 2439-solution.js

---
+++ Baekjoon-Algorithm/2439/2439-solution.js
@@ -0,0 +1,30 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input.push(Number(line));
+  // input = line.split(" ").map((el) => Number(el));
+  // input.push(line.split(" ").map((el) => Number(el)));
+}).on("close", function () {
+  let output = "";
+
+  for (let i = 0; i < input[0]; i++) {
+    let check = "";
+    for (let j = 0; j < input[0] - 1 - i; j++) {
+      check += " ";
+    }
+    for (let n = 0; n < i + 1; n++) {
+      check += "*";
+    }
+    output += check + "\n";
+  }
+
+  console.log(output);
+
+  process.exit();
+});
