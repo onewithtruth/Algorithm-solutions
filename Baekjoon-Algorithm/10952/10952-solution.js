cd10ad2bf570995908cb03c09c7b41599d1b3833
Hojin Jang <onewithtruth@gmail.com>
2021-10-10 18:56:11 +0900

    Create 10952-solution.js

---
+++ Baekjoon-Algorithm/10952/10952-solution.js
@@ -0,0 +1,25 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  // input.push(Number(line));
+  // input = line.split(" ").map((el) => Number(el));
+  input.push(line.split(" ").map((el) => Number(el)));
+}).on("close", function () {
+  let output = "";
+
+  let i = 0;
+  while (i < input.length - 1) {
+    output += input[i][0] + input[i][1] + "\n";
+    i++;
+  }
+
+  console.log(output);
+
+  process.exit();
+});
