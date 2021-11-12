ba16aaa719a3596aa3e37f7b51704764f3ee9d82
Hojin Jang <onewithtruth@gmail.com>
2021-11-12 10:06:19 +0900

    Update toy03_solution.js

--- Codestates-Algorithm/toy03/toy03_solution.js
+++ Codestates-Algorithm/toy03/toy03_solution.js
@@ -17,83 +17,82 @@ const isSubsetOf = function (base, sample) {
   };
 
 
-  class BinarySearchTree {
-    //BST의 constructor를 구현합니다.
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
-    // tree에 value를 추가합니다.
-    insert(value) {
-      // 인자의 value가 this.value보다 작을 경우, 왼쪽 노드에서 진행합니다.
-      if (value < this.value) {
-        // this.left에 아무것도 없을 경우, 새로운 자식 노드를 추가합니다.
-        if (this.left === null) {
-          this.left = new BinarySearchTree(value);
-        }
-        // this.left의 자식 노드가 있을 경우, 자식 노드에서 insert 재귀를 사용합니다.
-        else {
-          this.left.insert(value);
-        }
-      }
-      // 인자의 value가 this.value보다 클 경우, 오른쪽 노드에서 진행합니다.
-      else if (value > this.value) {
-        // this.right에 아무것도 없을 경우, 새로운 자식 노드를 추가합니다.
-        if (this.right === null) {
-          this.right = new BinarySearchTree(value);
-        }
-        // this.left의 자식 노드가 있을 경우, 자식 노드에서 insert 재귀를 사용합니다.
-        else {
-          this.right.insert(value);
-        }
-      } else {
-        // 이미 value값을 포함하고 있습니다.
-      }
-    }
-    // tree의 value값을 탐색합니다.
-    contains(value) {
-      // 찾는 value값이 노드의 value와 일치한다면, true를 리턴합니다.
-      if (value === this.value) {
-        return true;
-      }
-      // 찾는 value값이 노드의 value 보다 작다면, 왼쪽에서 contains의 재귀를 진행합니다.
-      if (value < this.value) {
-        return !!(this.left && this.left.contains(value));
-      }
-      // 찾는 value값이 노드의 value 보다 크다면, 오른쪽에서 contains의 재귀를 진행합니다.
-      if (value > this.value) {
-        return !!(this.right && this.right.contains(value));
-      }
-    }
-    //tree를 전위 순회 합니다.
-    preorder(callback) {
-      callback(this.value);
-      if (this.left) {
-        this.left.preorder(callback);
-      }
-      if (this.right) {
-        this.right.preorder(callback);
-      }
-    }
-    // tree를 중위 순회 합니다
-    inorder(callback) {
-      if (this.left) {
-        this.left.inorder(callback);
-      }
-      callback(this.value);
-      if (this.right) {
-        this.right.inorder(callback);
-      }
-    }
-    //tree를 후위 순회 합니다
-    postorder(callback) {
-      if (this.left) {
-        this.left.postorder(callback);
-      }
-      if (this.right) {
-        this.right.postorder(callback);
-      }
-      callback(this.value);
-    }
-  }
+// undirected graph (무향 그래프)
+// adjacency list (인접 리스트)
+class GraphWithAdjacencyList {
+	constructor() {
+		this.vertices = {};
+	}
+
+	addVertex(vertex) {
+		// 이미 존재하는 정점이라면 덮어씌워지지 않게 방지합니다.
+		this.vertices[vertex] = this.vertices[vertex] || [];
+	}
+
+	contains(vertex) {
+		// 인자로 넘겨받은 정점의 존재여부를 반환합니다.
+		return !!this.vertices[vertex];
+	}
+
+	addEdge(fromVertex, toVertex) {
+		// 넘겨받은 두 정점중 하나라도 존재하지 않는다면
+		if (!this.contains(fromVertex) || !this.contains(toVertex)) {
+			// 아무것도 하지않고 종료합니다
+			return;
+		}
+
+		// 두 정점이 모두 존재한다면
+		// 첫번째 정점의 인접 리스트에 두번째 정점을 추가하고 (간선이 존재하지 않을 경우)
+		if (!this.hasEdge(fromVertex, toVertex)) {
+			this.vertices[fromVertex].push(toVertex);
+		}
+		// 두번째 정점의 인접 리스트에 첫번째 정점을 추가합니다 (간선이 존재하지 않을 경우)
+		if (!this.hasEdge(toVertex, fromVertex)) {
+			this.vertices[toVertex].push(fromVertex);
+		}
+	}
+
+	hasEdge(fromVertex, toVertex) {
+		// 만약 정점(fromVertex)이 존재하지 않는다면
+		if (!this.contains(fromVertex)) {
+			// false를 반환합니다
+			return false;
+		}
+		// 존재한다면 해당 정점의 리스트에 toVertex가 포함되어있는지 반환합니다
+		return !!this.vertices[fromVertex].includes(toVertex);
+	}
+
+	removeEdge(fromVertex, toVertex) {
+		// 넘겨받은 두 정점중 하나라도 존재하지 않는다면
+		if (!this.contains(fromVertex) || !this.contains(toVertex)) {
+			// 아무것도 하지않고 종료합니다
+			return;
+		}
+
+		// 두 정점이 모두 존재한다면
+		// 첫번째 정점의 인접 리스트에 두번째 정점이 있을 경우
+		if (this.hasEdge(fromVertex, toVertex)) {
+			// 두번째 정점의 인덱스를 찾은 뒤 삭제합니다
+			const toVertexIndex = this.vertices[fromVertex].indexOf(toVertex);
+			this.vertices[fromVertex].splice(toVertexIndex, 1);
+		}
+		// 두번째 정점의 인접 리스트에 첫번째 정점이 있을 경우
+		if (this.hasEdge(toVertex, fromVertex)) {
+			// 첫번째 정점의 인덱스를 찾은 뒤 삭제합니다
+			const fromVertexIndex = this.vertices[toVertex].indexOf(fromVertex);
+			this.vertices[toVertex].splice(fromVertexIndex, 1);
+		}
+	}
+
+	removeVertex(vertex) {
+		// 만약 인자로 넘겨받은 정점이 존재한다면
+		if (this.contains(vertex)) {
+			// 해당 정점과 연결된 간선을 지우고
+			while (this.vertices[vertex].length > 0) {
+				this.removeEdge(this.vertices[vertex][0], vertex);
+			}
+			// 최종적으로 해당 정점을 삭제합니다
+			delete this.vertices[vertex];
+		}
+	}
+}
