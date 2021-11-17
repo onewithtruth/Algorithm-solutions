d3bc3453aebc7838046bcfdc80417703b0f61ba0
Hojin Jang <onewithtruth@gmail.com>
2021-11-18 01:46:51 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -17,80 +17,26 @@ rl.on("line", function (line) {
 });
 
 
-const sudoku = function (board) {
-  const N = board.length;
-  const boxes = [
-    [0, 0, 0, 1, 1, 1, 2, 2, 2],
-    [0, 0, 0, 1, 1, 1, 2, 2, 2],
-    [0, 0, 0, 1, 1, 1, 2, 2, 2],
-    [3, 3, 3, 4, 4, 4, 5, 5, 5],
-    [3, 3, 3, 4, 4, 4, 5, 5, 5],
-    [3, 3, 3, 4, 4, 4, 5, 5, 5],
-    [6, 6, 6, 7, 7, 7, 8, 8, 8],
-    [6, 6, 6, 7, 7, 7, 8, 8, 8],
-    [6, 6, 6, 7, 7, 7, 8, 8, 8],
-  ];
-  const getBoxNum = (row, col) => boxes[row][col];
+let dfs = function (node) {
+  // TODO: 여기에 코드를 작성합니다.
+  let values = [node.value];
 
-  const blanks = [];
-  const rowUsed = [];
-  const colUsed = [];
-  const boxUsed = [];
-  for (let row = 0; row < N; row++) {
-    rowUsed.push(Array(N + 1).fill(false));
-    colUsed.push(Array(N + 1).fill(false));
-    boxUsed.push(Array(N + 1).fill(false));
-  }
+  node.children.forEach((n) => {
+    values = values.concat(dfs(n));
+  });
 
-  for (let row = 0; row < N; row++) {
-    for (let col = 0; col < N; col++) {
-      if (board[row][col] === 0) {
-        blanks.push([row, col]);
-      } else {
-        const num = board[row][col];
-        const box = getBoxNum(row, col);
-        rowUsed[row][num] = true;
-        colUsed[col][num] = true;
-        boxUsed[box][num] = true;
-      }
-    }
-  }
+  return values;
+};
 
-  const isValid = (row, col, num) => {
-    const box = getBoxNum(row, col);
-    return (
-      rowUsed[row][num] === false &&
-      colUsed[col][num] === false &&
-      boxUsed[box][num] === false
-    );
-  };
+// 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
+let Node = function (value) {
+  this.value = value;
+  this.children = [];
+};
 
-  const toggleNum = (row, col, num) => {
-    const box = getBoxNum(row, col);
-    board[row][col] = num;
-    rowUsed[row][num] = !rowUsed[row][num];
-    colUsed[col][num] = !colUsed[col][num];
-    boxUsed[box][num] = !boxUsed[box][num];
-  };
-
-  const aux = (idx, blanks, board) => {
-    if (idx === blanks.length) {
-      return true;
-    }
-
-    const [row, col] = blanks[idx];
-    for (let num = 1; num <= 9; num++) {
-      if (isValid(row, col, num) === true) {
-        toggleNum(row, col, num);
-        if (aux(idx + 1, blanks, board) === true) {
-          return true;
-        }
-        toggleNum(row, col, num);
-      }
-    }
-    return false;
-  };
-
-  aux(0, blanks, board);
-  return board;
+// 위 Node 객체로 구성되는 트리는 매우 단순한 형태의 트리입니다.
+// membership check(중복 확인)를 따로 하지 않습니다.
+Node.prototype.addChild = function (child) {
+  this.children.push(child);
+  return child;
 };
