8da5192f8574fa28c16d114814c27e1aa0a06830
Hojin Jang <onewithtruth@gmail.com>
2021-11-18 16:44:09 +0900

    Update toy08_solution.js

--- Codestates-Algorithm/toy08/toy08_solution.js
+++ Codestates-Algorithm/toy08/toy08_solution.js
@@ -70,3 +70,12 @@
 
 // };
 
+// !!reference below
+
+const largestProductOfThree = function (arr) {
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const len = arr.length;
+  const candi1 = sorted[len - 1] * sorted[len - 2] * sorted[len - 3];
+  const candi2 = sorted[len - 1] * sorted[0] * sorted[1];
+  return Math.max(candi1, candi2);
+};
