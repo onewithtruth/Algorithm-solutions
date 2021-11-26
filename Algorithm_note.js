5d61d1c2e8bf6f958ca85f27aaa7ac019c767f16
Hojin Jang <onewithtruth@gmail.com>
2021-11-26 14:04:28 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -17,30 +17,22 @@ rl.on("line", function (line) {
 });
 
 
-let bfs = function (node) {
-  // TODO: 여기에 코드를 작성합니다.
-  let queue = [node];
-  const values = [];
-  while (queue.length > 0) {
-    const head = queue[0];
-    queue = queue.slice(1);
-
-    values.push(head.value);
-
-    head.children.forEach((child) => queue.push(child));
+const insertionSort = function (arr, transform = (item) => item) {
+  let sorted = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (transform(arr[i]) >= transform(sorted[i - 1])) {
+      sorted.push(arr[i]);
+    } else {
+      for (let j = 0; j < i; j++) {
+        if (transform(arr[i]) <= transform(sorted[j])) {
+          const left = sorted.slice(0, j);
+          const right = sorted.slice(j);
+          sorted = left.concat(arr[i], right);
+          break;
+        }
+      }
+    }
   }
-  return values;
-};
 
-// 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
-let Node = function (value) {
-  this.value = value;
-  this.children = [];
-};
-
-// 위 Node 객체로 구성되는 트리는 매우 단순한 형태의 트리입니다.
-// membership check(중복 확인)를 따로 하지 않습니다.
-Node.prototype.addChild = function (child) {
-  this.children.push(child);
-  return child;
+  return sorted;
 };
