2f9eb5f5e908ca5019a3d3b169aa4ccaaee10742
Hojin Jang <onewithtruth@gmail.com>
2021-11-29 22:56:02 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -17,38 +17,85 @@ rl.on("line", function (line) {
 });
 
 
-function createMatrix(edges) {
+const isPrime = (num) => {
+  if (num % 2 === 0) return false;
+  let sqrt = parseInt(Math.sqrt(num));
+  for (let divider = 3; divider <= sqrt; divider += 2) {
+    if (num % divider === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
-	// 행렬의 크기를 구합니다.
-	// max 변수를 0으로 할당하고, edges를 전부 순회해 제일 큰 숫자를 max에 할당합니다.
-	// max보다 크지 않을 경우엔 바꾸지 않습니다.
-	let max = 0;
-	for (let i = 0; i < edges.length; i++) {
-		// edges의 한 요소에는 숫자 이외에 방향성도 있으니, 숫자까지만 slice를 하여 비교합니다.
-		const curMax = Math.max(...edges[i].slice(0, 2));
-		curMax > max ? (max = curMax) : null;
-	}
+// 4자리 수를 받아서 각 자리수의 수들의 배열로 변환하는 함수
+//  let output = splitNum(3359);
+//  console.log(output); // --> [3, 3, 5, 9]
+const splitNum = (num) => {
+  const digits = num.toString().split('');
+  return digits.map((d) => Number(d));
+};
 
-  // matrix의 뼈대를 잡습니다.
-  // max로 구한 최대값에 1을 더하여 array를 만들고(0번째부터 있기 때문입니다) 전부 0으로 채운 뒤
-	// map을 사용해 각 요소마다 배열로 바꿔줍니다. 배열의 크기를 이번에도 최대값에 1을 더한 뒤, 0으로 채웁니다.
-	const result = new Array(max + 1).fill(0).map((row) => new Array(max + 1).fill(0));
-	
-	// !!! new Array(max + 1).fill(new Array(max + 1))처럼 하게 되면, 주소를 참조하게 됩니다. 꼭 0을 채운 뒤, Map으로 바꾸는 등
-	// 주소를 참조하지 않는 방법을 사용해야 합니다 !!!
+// 길이의 4의 수 배열을 받아서, 4자리의 수로 변환하는 함수
+//  let output = splitNum([3, 3, 5, 9]);
+//  console.log(output); // --> 3359
+const joinDigits = (digits) => Number(digits.join(''));
 
-  // edges를 순회하며 무향인 곳엔 각각의 간선에 1로 바꾸어 주고, 방향이 있는 간선엔 해당 간선에만 1로 바꾸어 줍니다.
-	// 만약, [0, 3, "directed"]가 들어왔다면,
-	// 만들어 둔 result의 0 번째 row에 3 번째 col에 1을 추가합니다.
-	// [ 0, 0, 0, 1 ] => 0번째 버텍스가 갈 수 있는 간선 중, 3 번으로 가는 간선만 갈 수 있습니다.
-	edges.forEach((edge) => {
-		const [row, col, direction] = edge;
-		if (direction === "undirected") {
-			result[col][row] = 1;
-		}
-		result[row][col] = 1;
-	});
+const primePassword = (curPwd, newPwd) => {
+  if (curPwd === newPwd) return 0;
+  // bfs를 위해 queue를 선언
+  let front = 0;
+  let rear = 0;
+  const queue = [];
+  const isEmpty = (queue) => front === rear;
+  const enQueue = (queue, item) => {
+    queue.push(item);
+    rear++;
+  };
+  const deQueue = (queue) => {
+    return queue[front++];
+    // const item = queue[front];
+    // front++;
+    // return item;
+  };
 
-  // result를 반환합니다.
-	return result;
-}
+  // 각 4자리의 방문 여부를 저장하는 배열
+  // 한 번 방문한 수(가장 최소의 동작으로 만든 수)는 다시 방문할 필요가 없다.
+  const isVisited = Array(10000).fill(false);
+  isVisited[curPwd] = true;
+  // bfs를 위한 시작점
+  // 큐에는 [필요한 동작 수, 비밀번호]가 저장된다.
+  enQueue(queue, [0, curPwd]);
+  // bfs는 큐가 빌(empty) 때까지 탐색한다.
+  while (isEmpty(queue) === false) {
+    const [step, num] = deQueue(queue);
+    // 각 자리수 마다 변경이 가능하므로 4번의 반복이 필요하다.
+    for (let i = 0; i < 4; i++) {
+      const digits = splitNum(num);
+      // 0부터 9까지 시도한다.
+      for (let d = 0; d < 10; d++) {
+        // 각 자리수마다 원래 있던 수(digits[i])는 피해야 한다.
+        if (d !== digits[i]) {
+          // 현재 자리수의 수를 변경하고,
+          digits[i] = d;
+          // 변경한 후 4자리 수를 구한다.
+          const next = joinDigits(digits);
+          // 만약 이 수가 새 비밀번호와 같다면 리턴한다.
+          // next는 deQueue된 num으로부터 1단계 다음에 도달한 수이다.
+          if (next === newPwd) return step + 1;
+          // 1,000보다 큰 소수여야 하고, 방문된 적이 없어야 한다.
+          if (next > 1000 && isPrime(next) && isVisited[next] === false) {
+            // 방문 여부를 표시하고,
+            isVisited[next] = true;
+            // 큐에 넣는다.
+            enQueue(queue, [step + 1, next]);
+          }
+        }
+      }
+    }
+  }
+
+  // 큐가 빌 때까지, 즉 모든 경우의 수를 탐색할 때까지 리턴되지 않은 경우
+  // 현재 비밀번호에서 새 비밀번호를 만들 수 없다.
+  return -1;
+};
