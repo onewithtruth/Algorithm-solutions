866080f00b04628c5c1291335a803d16337862bc
Hojin Jang <onewithtruth@gmail.com>
2021-12-01 12:59:52 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -18,24 +18,7 @@ rl.on("line", function (line) {
 
 
 // naive solution
-// const quickSort = function (arr) {
-//   if (arr.length <= 1) return arr;
-
-//   const pivot = arr[0];
-//   const left = [];
-//   const right = [];
-
-//   for (let i = 1; i < arr.length; i++) {
-//     if (arr[i] <= pivot) left.push(arr[i]);
-//     else right.push(arr[i]);
-//   }
-
-//   const lSorted = quickSort(left);
-//   const rSorted = quickSort(right);
-//   return [...lSorted, pivot, ...rSorted];
-// };
-
-function quickSort(arr, transform = (item) => item) {
+const quickSort = function (arr) {
   if (arr.length <= 1) return arr;
 
   const pivot = arr[0];
@@ -43,11 +26,28 @@ function quickSort(arr, transform = (item) => item) {
   const right = [];
 
   for (let i = 1; i < arr.length; i++) {
-    if (transform(arr[i]) < transform(pivot)) left.push(arr[i]);
+    if (arr[i] <= pivot) left.push(arr[i]);
     else right.push(arr[i]);
   }
 
-  const lSorted = quickSort(left, transform);
-  const rSorted = quickSort(right, transform);
+  const lSorted = quickSort(left);
+  const rSorted = quickSort(right);
   return [...lSorted, pivot, ...rSorted];
-}
+};
+
+// function quickSort(arr, transform = (item) => item) {
+//   if (arr.length <= 1) return arr;
+
+//   const pivot = arr[0];
+//   const left = [];
+//   const right = [];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (transform(arr[i]) < transform(pivot)) left.push(arr[i]);
+//     else right.push(arr[i]);
+//   }
+
+//   const lSorted = quickSort(left, transform);
+//   const rSorted = quickSort(right, transform);
+//   return [...lSorted, pivot, ...rSorted];
+// }
