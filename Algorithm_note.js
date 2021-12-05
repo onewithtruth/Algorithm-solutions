12c024e083410453ad4888ed380f313bc734c4a1
Hojin Jang <onewithtruth@gmail.com>
2021-12-06 01:22:13 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -16,110 +16,83 @@ rl.on("line", function (line) {
   process.exit();
 });
 
-
-// naive solution
-// const LPS3 = function (str) {
-//   if (str.length < 2) return 0;
-
-//   // 문자열을 두 부분으로 나누고
-//   // 부분 문자열을 쉽게 구하기 위해
-//   // 왼쪽 부분의 마지막 인덱스와 오른쪽 부분의 첫 인덱스를 저장
-
-//   let halfSize = Math.floor(str.length / 2);
-//   // 문자열의 길이가 홀수일 수 있으므로, 올림한다.
-//   let rightStart = Math.ceil(str.length / 2);
-
-//   // 가장 긴 LPS 후보부터 차례대로 검사한다
-//   for (let offset = 0; offset < halfSize; offset++) {
-//     let matched = true;
-//     for (let i = 0; i < halfSize - offset; i++) {
-//       if (str[i] !== str[rightStart + offset + i]) {
-//         matched = false;
-//         break;
-//       }
-//     }
-//     if (matched) return halfSize - offset;
-//   }
-
-//   // LPS가 없는 경우
-//   return 0;
-// };
-
-// naive solution2
-// const LPS = function (str) {
-//   if (str.length < 2) return 0;
-
-//   // 문자열을 두 부분으로 나누고
-//   // 각 부분의 첫 인덱스를 저장
-//   let leftIdx = 0;
-//   // 문자열의 길이가 홀수일 수 있으므로, 올림한다.
-//   let rightIdx = Math.ceil(str.length / 2);
-
-//   while (rightIdx < str.length) {
-//     if (str[leftIdx] == str[rightIdx]) {
-//       // LPS 검사를 시작한 위치부터 지금까지 계속 같은 경우
-//       // 다음 문자도 같은지 확인하기 위해 인덱스를 이동한다.
-//       leftIdx++;
-//       rightIdx++;
-//     } else {
-//       // leftIdx가 0인 경우, 단순히 rightIdx를 1 증가 (suffix의 시작점을 뒤로 한 칸 이동)
-//       // prefix, suffix가 계속 일치하다가 중간에서 일치하지 않는 경우에도,
-//       // 현재 suffix의 시작점을 뒤로 한 칸 이동한다.
-//       rightIdx = rightIdx - leftIdx + 1;
-//       leftIdx = 0;
-//     }
-//   }
-
-//   // LPS가 없는 경우
-//   return leftIdx;
-// };
-
-// dynamic solution: O(N)
-// non-overlapping 조건을 제거하고 lps를 구한다.
-// lps는 주어진 문자열에서 아래 조건을 만족하는 가장 긴 접두어(prefix)의 길이를 의미한다.
-//  - 해당 접두어는 주어진 문자열의 접미어(suffix)이기도 하다.
-// 이때, 문자열 자기 자신은 그 자체로 prefix이자 suffix인데, 이는 고려 대상에서 제외한다.
-const LPS = function (str) {
-  // lps[i]는 0부터 i까지의 부분 문자열, 즉 str.slice(0, i + 1)에서 lps의 길이를 저장한다.
-  const lps = Array(str.length);
-  // lps[0]은 길이가 1인 문자열의 lps의 길이이므로 항상 0이다. (자기 자신 포함 금지)
-  lps[0] = 0;
-  let leftIdx = 0;
-  let rightIdx = 1;
-  // lps[i]를 1부터, 즉 문자열의 길이가 2일때부터 차례대로 구한다.
-  while (rightIdx < str.length) {
-    if (str[leftIdx] === str[rightIdx] && rightIdx >= str.length / 2) {
-      // 가장 단순한 경우를 생각해보면, 쉽게 이해할 수 있다.
-      // 1) 길이가 2 경우
-      // 2) 길이가 3 이상인데 전부 같은 문자인 경우
-      // 0부터 leftIdx까지 매칭이 되었으므로 매칭된 길이는 leftIdx + 1이다.
-      leftIdx++;
-      lps[rightIdx] = leftIdx;
-      rightIdx++;
-    } else {
-      // 중간에 매칭이 되지 않은 경우, leftIdx를 조정한다.
-      // 현재 lps[0]부터 lps[rightIdx - 1]까지 계산이 완료된 상태이다.
-      // 처음부터 다시 prefix, suffix 매칭을 하는 것이 원칙이지만
-      // 앞서 계산한 결과인 lps 배열을 통해 처음으로 되돌아갈 필요는 없다.
-
-      // 예. aaabaaaa
-      // 현재 leftIdx는 2, rigthIdx는 3, lps는 [0, 1, 2]인 상태라고 가정해보자.
-      // leftIdx가 1일 때까지, 즉 현재 leftIdx 직전(leftIdx - 1)까지는 매칭이 되었다.
-      if (leftIdx !== 0) {
-        leftIdx = lps[leftIdx - 1];
-        // Also, note that we do
-        // not increment i here
-      } else {
-        // rightIdx가 1인 경우, 즉 첫 iteration일 경우
-        // lps[rightIdx]가 0인 것은 명백하다. (예. ab)
-        // leftIdx가 0이라는 것은 처음부터 prefix, suffix 매칭을 하는 경우이다.
-        //
-        // lps가 존재하지 않는 경우이다.
-        lps[rightIdx] = 0;
-        rightIdx++;
+class BinarySearchTree {
+  //BST의 constructor를 구현합니다.
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+  // tree에 value를 추가합니다.
+  insert(value) {
+    // 인자의 value가 this.value보다 작을 경우, 왼쪽 노드에서 진행합니다.
+    if (value < this.value) {
+      // this.left에 아무것도 없을 경우, 새로운 자식 노드를 추가합니다.
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      }
+      // this.left의 자식 노드가 있을 경우, 자식 노드에서 insert 재귀를 사용합니다.
+      else {
+        this.left.insert(value);
       }
     }
+    // 인자의 value가 this.value보다 클 경우, 오른쪽 노드에서 진행합니다.
+    else if (value > this.value) {
+      // this.right에 아무것도 없을 경우, 새로운 자식 노드를 추가합니다.
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      }
+      // this.left의 자식 노드가 있을 경우, 자식 노드에서 insert 재귀를 사용합니다.
+      else {
+        this.right.insert(value);
+      }
+    } else {
+      // 이미 value값을 포함하고 있습니다.
+    }
   }
-  const res = lps[lps.length - 1];
-  return res > str.length / 2 ? Math.floor(str.length / 2) : res;
-};
+  // tree의 value값을 탐색합니다.
+  contains(value) {
+    // 찾는 value값이 노드의 value와 일치한다면, true를 리턴합니다.
+    if (value === this.value) {
+      return true;
+    }
+    // 찾는 value값이 노드의 value 보다 작다면, 왼쪽에서 contains의 재귀를 진행합니다.
+    if (value < this.value) {
+      return !!(this.left && this.left.contains(value));
+    }
+    // 찾는 value값이 노드의 value 보다 크다면, 오른쪽에서 contains의 재귀를 진행합니다.
+    if (value > this.value) {
+      return !!(this.right && this.right.contains(value));
+    }
+  }
+  //tree를 전위 순회 합니다.
+  preorder(callback) {
+    callback(this.value);
+    if (this.left) {
+      this.left.preorder(callback);
+    }
+    if (this.right) {
+      this.right.preorder(callback);
+    }
+  }
+  // tree를 중위 순회 합니다
+  inorder(callback) {
+    if (this.left) {
+      this.left.inorder(callback);
+    }
+    callback(this.value);
+    if (this.right) {
+      this.right.inorder(callback);
+    }
+  }
+  //tree를 후위 순회 합니다
+  postorder(callback) {
+    if (this.left) {
+      this.left.postorder(callback);
+    }
+    if (this.right) {
+      this.right.postorder(callback);
+    }
+    callback(this.value);
+  }
+}
