7ac5198b24ff9ab7e40c4e328e7e40aae78838e5
Hojin Jang <onewithtruth@gmail.com>
2021-12-09 20:19:24 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -16,83 +16,47 @@ rl.on("line", function (line) {
   process.exit();
 });
 
-class BinarySearchTree {
-  //BST의 constructor를 구현합니다.
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-  // tree에 value를 추가합니다.
-  insert(value) {
-    // 인자의 value가 this.value보다 작을 경우, 왼쪽 노드에서 진행합니다.
-    if (value < this.value) {
-      // this.left에 아무것도 없을 경우, 새로운 자식 노드를 추가합니다.
-      if (this.left === null) {
-        this.left = new BinarySearchTree(value);
+const inequalityNumber = function (signs) {
+  const aux = (idx, signs, nums, digits, isVisited) => {
+    if (idx === signs.length) {
+      // 부등호 수를 만든 경우
+      return parseInt(nums.join(''));
+    }
+
+    const sign = signs[idx];
+    for (let i = 0; i < digits.length; i++) {
+      // 숫자를 차례대로 검토한다.
+      // max를 구할 때는 9부터, min을 구할 때는 0부터
+      const right = digits[i];
+      // 이전 단계에서 사용한 숫자인 경우 스킵
+      if (isVisited[right]) continue;
+
+      // 첫번째 숫자가 아닌 경우에는 조건이 중요하다.
+      if (idx >= 0) {
+        // 항상 바로 직전의 숫자와 비교하면 된다.
+        const left = nums[nums.length - 1];
+        if (sign === '<' && left >= right) continue;
+        if (sign === '>' && left <= right) continue;
       }
-      // this.left의 자식 노드가 있을 경우, 자식 노드에서 insert 재귀를 사용합니다.
-      else {
-        this.left.insert(value);
+
+      // 조건을 만족하거나 첫번째 숫자인 경우
+      isVisited[right] = true;
+      const target = aux(idx + 1, signs, nums.concat(right), digits, isVisited);
+      if (target !== undefined) {
+        // 부등호 수를 이미 찾은 경우 탐색을 더 할 필요가 없다.
+        return target;
       }
+      // 탐색에 실패한 경우, 시도한 숫자의 상태(사용중)를 원래대로(사용안함) 바꿔놔야 한다.
+      isVisited[right] = false;
     }
-    // 인자의 value가 this.value보다 클 경우, 오른쪽 노드에서 진행합니다.
-    else if (value > this.value) {
-      // this.right에 아무것도 없을 경우, 새로운 자식 노드를 추가합니다.
-      if (this.right === null) {
-        this.right = new BinarySearchTree(value);
-      }
-      // this.left의 자식 노드가 있을 경우, 자식 노드에서 insert 재귀를 사용합니다.
-      else {
-        this.right.insert(value);
-      }
-    } else {
-      /// 이미 value값을 포함하고 있습니다.
-    }
-  }
-  // tree의 value값을 탐색합니다.
-  contains(value) {
-    // 찾는 value값이 노드의 value와 일치한다면, true를 리턴합니다.
-    if (value === this.value) {
-      return true;
-    }
-    // 찾는 value값이 노드의 value 보다 작다면, 왼쪽에서 contains의 재귀를 진행합니다.
-    if (value < this.value) {
-      return !!(this.left && this.left.contains(value));
-    }
-    // 찾는 value값이 노드의 value 보다 크다면, 오른쪽에서 contains의 재귀를 진행합니다.
-    if (value > this.value) {
-      return !!(this.right && this.right.contains(value));
-    }
-  }
-  //tree를 전위 순회 합니다.
-  preorder(callback) {
-    callback(this.value);
-    if (this.left) {
-      this.left.preorder(callback);
-    }
-    if (this.right) {
-      this.right.preorder(callback);
-    }
-  }
-  // tree를 중위 순회 합니다
-  inorder(callback) {
-    if (this.left) {
-      this.left.inorder(callback);
-    }
-    callback(this.value);
-    if (this.right) {
-      this.right.inorder(callback);
-    }
-  }
-  //tree를 후위 순회 합니다
-  postorder(callback) {
-    if (this.left) {
-      this.left.postorder(callback);
-    }
-    if (this.right) {
-      this.right.postorder(callback);
-    }
-    callback(this.value);
-  }
-}
+
+    return undefined;
+  };
+
+  signs = signs.split(' ');
+  const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // arr.reverse()는 in-place 함수(데이터 직접 변경)이므로 min과 max의 순서는 중요하다.
+  const min = aux(-1, signs, [], digits, Array(10).fill(false));
+  const max = aux(-1, signs, [], digits.reverse(), Array(10).fill(false));
+  return max - min;
+};
