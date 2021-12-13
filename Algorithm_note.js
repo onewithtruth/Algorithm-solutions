f378d514f744b6495f067ae5d10777d77e68272e
Hojin Jang <onewithtruth@gmail.com>
2021-12-13 20:50:35 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -16,47 +16,44 @@ rl.on("line", function (line) {
   process.exit();
 });
 
-const inequalityNumber = function (signs) {
-  const aux = (idx, signs, nums, digits, isVisited) => {
-    if (idx === signs.length) {
-      // 부등호 수를 만든 경우
-      return parseInt(nums.join(''));
+const spiralTraversal = function (matrix) {
+  // 각 방향마다 row와 col의 변화를 저장
+  const RIGHT = [0, 1];
+  const DOWN = [1, 0];
+  const LEFT = [0, -1];
+  const UP = [-1, 0];
+  // 각 방향을 위한 lookup table
+  const MOVES = [RIGHT, DOWN, LEFT, UP];
+  const M = matrix.length;
+  const N = matrix[0].length;
+  const isValid = (row, col) => row >= 0 && row < M && col >= 0 && col < N;
+
+  let cnt = 0;
+  let row = 0,
+    col = -1;
+  let direction = 0;
+  let result = '';
+  while (cnt < M * N) {
+    const move = MOVES[direction];
+    const [rd, cd] = move;
+
+    row = row + rd;
+    col = col + cd;
+    while (isValid(row, col) && matrix[row][col] !== false) {
+      result = result + matrix[row][col];
+      // 한 요소를 두 번 접근하지 않게 하기 위해서, 접근된 요소를 false로 변경한다.
+      matrix[row][col] = false;
+      row = row + rd;
+      col = col + cd;
+      cnt++;
     }
+    // row, col 이 행렬의 범위를 벗어났기 때문에,
+    // 진행된 방향의 반대로 한 칸 이동한다.
+    row = row - rd;
+    col = col - cd;
 
-    const sign = signs[idx];
-    for (let i = 0; i < digits.length; i++) {
-      // 숫자를 차례대로 검토한다.
-      // max를 구할 때는 9부터, min을 구할 때는 0부터
-      const right = digits[i];
-      // 이전 단계에서 사용한 숫자인 경우 스킵
-      if (isVisited[right]) continue;
-
-      // 첫번째 숫자가 아닌 경우에는 조건이 중요하다.
-      if (idx >= 0) {
-        // 항상 바로 직전의 숫자와 비교하면 된다.
-        const left = nums[nums.length - 1];
-        if (sign === '<' && left >= right) continue;
-        if (sign === '>' && left <= right) continue;
-      }
-
-      // 조건을 만족하거나 첫번째 숫자인 경우
-      isVisited[right] = true;
-      const target = aux(idx + 1, signs, nums.concat(right), digits, isVisited);
-      if (target !== undefined) {
-        // 부등호 수를 이미 찾은 경우 탐색을 더 할 필요가 없다.
-        return target;
-      }
-      // 탐색에 실패한 경우, 시도한 숫자의 상태(사용중)를 원래대로(사용안함) 바꿔놔야 한다.
-      isVisited[right] = false;
-    }
-
-    return undefined;
-  };
-
-  signs = signs.split(' ');
-  const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  // arr.reverse()는 in-place 함수(데이터 직접 변경)이므로 min과 max의 순서는 중요하다.
-  const min = aux(-1, signs, [], digits, Array(10).fill(false));
-  const max = aux(-1, signs, [], digits.reverse(), Array(10).fill(false));
-  return max - min;
+    // 각 방향이 순환되기 때문에 모듈러 연산을 사용한다.
+    direction = (direction + 1) % 4;
+  }
+  return result;
 };
