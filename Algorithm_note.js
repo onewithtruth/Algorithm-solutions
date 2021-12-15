f8affa29bd89504b89949b60ef729eb82939df09
Hojin Jang <onewithtruth@gmail.com>
2021-12-16 00:29:52 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -16,45 +16,42 @@ rl.on("line", function (line) {
   process.exit();
 });
 
-// const rotateMatrix = function (matrix) {
-//   const N = matrix.length;
-//   const M = matrix[0] && matrix[0].length;
-//   let output = [];
+function getMax(arr) {
+  return arr.reduce((max, item) => {
+    if (item > max) return item;
+    return max;
+  }, 0);
+}
 
-//   for (let row = 0; row < M; row++) {
-//     output[row] = [];
-//     for (let col = 0; col < N; col++) {
-//       output[row][col] = matrix[N - col - 1][row];
-//     }
-//   }
+function countingSort(arr, radix) {
+  const N = arr.length;
+  const output = Array(N).fill(0);
+  const count = Array(10).fill(0);
 
-//   return output;
-// };
+  // 현재 자리수를 기준으로 0~9의 개수를 센다.
+  arr.forEach((item) => {
+    const idx = Math.floor(item / radix) % 10;
+    count[idx]++;
+  });
 
-const rotateMatrix = function (matrix, rotateNum = 1) {
-  // rotateNum 이 0일 수 있으므로 아래와 같은 초기화는 지양해야 함
-  // rotateNum = rotateNum || 1
-  const N = matrix.length;
-  // 빈 배열을 입력받을 수 있다.
-  const M = matrix[0] && matrix[0].length;
+  // count[i]가 i까지의 누적 개수가 되도록 만든다.
+  count.reduce((totalNum, num, idx) => {
+    count[idx] = totalNum + num;
+    return totalNum + num;
+  });
 
-  rotateNum = rotateNum % 4;
-  // 회전하지 않는다.
-  if (rotateNum === 0) return matrix;
-
-  const rotated = [];
-  // 홀수번 회전 시 M x N, 짝수번 회전 시 N x M
-  const RC = rotateNum % 2 === 1 ? [M, N] : [N, M];
-
-  for (let row = 0; row < RC[0]; row++) {
-    rotated[row] = [];
-    for (let col = 0; col < RC[1]; col++) {
-      if (rotateNum === 1) rotated[row][col] = matrix[N - col - 1][row];
-      else if (rotateNum === 2)
-        rotated[row][col] = matrix[N - row - 1][M - col - 1];
-      else rotated[row][col] = matrix[col][M - row - 1];
-    }
+  // 아래 속성이 유지되도록 하기 위해 배열을 거꾸로 순회한다.
+  //  1. 가장 큰 값을 먼저 본다.
+  //  2. 가장 큰 값을 가장 마지막에 놓는다.
+  let i = N - 1;
+  while (i >= 0) {
+    const idx = Math.floor(arr[i] / radix) % 10;
+    // count[idx]: 현재 radix의 idx까지 누적 개수
+    // count[idx]개만큼 있으므로, 인덱스는 count[idx] - 1
+    output[count[idx] - 1] = arr[i];
+    count[idx] -= 1;
+    i--;
   }
 
-  return rotated;
-};
+  return output;
+}
