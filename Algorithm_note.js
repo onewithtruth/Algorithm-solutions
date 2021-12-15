f7b0457183e367d25221996e0e339fe392e92e12
Hojin Jang <onewithtruth@gmail.com>
2021-12-15 19:29:20 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -16,38 +16,45 @@ rl.on("line", function (line) {
   process.exit();
 });
 
-// 최대 공약수(유클리드 호제법: Euclidean algorithm)
-function gcd(m, n) {
-  if (m % n === 0) return n;
-  return gcd(n, m % n);
-}
+// const rotateMatrix = function (matrix) {
+//   const N = matrix.length;
+//   const M = matrix[0] && matrix[0].length;
+//   let output = [];
 
-function divideChocolateStick(M, N) {
-  const result = [];
-  // 최대공약수를 구한다.
-  // M, N의 순서는 상관없다.
-  const GCD = gcd(M, N);
-  let temp = 0; //
+//   for (let row = 0; row < M; row++) {
+//     output[row] = [];
+//     for (let col = 0; col < N; col++) {
+//       output[row][col] = matrix[N - col - 1][row];
+//     }
+//   }
 
-  // 약수는 대칭적이므로 제곱근까지만 반복해도 된다.
-  // 예) 36의 약수는 1, 2, 3, 4, 6, 9, 12, 18, 36이다.
-  // 제곱근을 기준으로 양쪽의 값 하나씩 곱했을 때 36이 되기 때문에
-  // 제곱근 보다 큰 약수는 제곱근보다 작은 약수에서 구할 수 있다.
-  const sqrt = Math.floor(Math.sqrt(GCD));
-  for (let left = 1; left <= sqrt; left++) {
-    if (GCD % left === 0) {
-      // 최대공약수의 약수인 경우 중 제곱근 보다 작은 약수의 경우
-      result.push([left, M / left, N / left]);
-      if (left * left < GCD) {
-        // 제곱근이 아닌 경우(제곱근 보다 작은)
-        right = GCD / left; // 최대 공약수를 제곱근이 아닌 수로 나누면 제곱근 보다 큰 약수를 구할 수 있다.
-        result.push([right, M / right, N / right]);
-      }
+//   return output;
+// };
+
+const rotateMatrix = function (matrix, rotateNum = 1) {
+  // rotateNum 이 0일 수 있으므로 아래와 같은 초기화는 지양해야 함
+  // rotateNum = rotateNum || 1
+  const N = matrix.length;
+  // 빈 배열을 입력받을 수 있다.
+  const M = matrix[0] && matrix[0].length;
+
+  rotateNum = rotateNum % 4;
+  // 회전하지 않는다.
+  if (rotateNum === 0) return matrix;
+
+  const rotated = [];
+  // 홀수번 회전 시 M x N, 짝수번 회전 시 N x M
+  const RC = rotateNum % 2 === 1 ? [M, N] : [N, M];
+
+  for (let row = 0; row < RC[0]; row++) {
+    rotated[row] = [];
+    for (let col = 0; col < RC[1]; col++) {
+      if (rotateNum === 1) rotated[row][col] = matrix[N - col - 1][row];
+      else if (rotateNum === 2)
+        rotated[row][col] = matrix[N - row - 1][M - col - 1];
+      else rotated[row][col] = matrix[col][M - row - 1];
     }
   }
 
-  // '빼빼로를 받게 되는 직원의 수'를 기준으로 오름차순으로 정렬
-  result.sort((op1, op2) => op1[0] - op2[0]);
-
-  return result;
-}
+  return rotated;
+};
