0d4118d391112a22590d6de2a76ecf0321e7388c
Hojin Jang <onewithtruth@gmail.com>
2021-12-22 22:25:54 +0900

    Create section-04-03.js

---
+++ inflearn-Algorithm/section-04/section-04-03.js
@@ -0,0 +1,16 @@
+
+function solution(test){
+let result
+for(let i = 0; i < arr.length; i++) {
+for(let n = 0; n < arr[0].length; n++) {
+if (arr[i][j])
+}
+}
+}
+
+let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
+console.log(solution(arr));
+
+function solution (test {
+            
+})
