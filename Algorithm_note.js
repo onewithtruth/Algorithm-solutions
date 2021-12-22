5979aa82e0fdb6c0056a28dadc0bf1220b88c30a
Hojin Jang <onewithtruth@gmail.com>
2021-12-22 15:38:04 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -16,24 +16,9 @@ rl.on("line", function (line) {
   process.exit();
 });
 
-// 아래 코드는 수정하지 마세요.
+
 function swap(idx1, idx2, arr) {
-  // 두 변수를 바꾸는 방법
-
-  // 1) 임시 변수를 활용한 방법
-  // let temp = arr[idx1];
-  // arr[idx1] = arr[idx2];
-  // arr[idx2] = temp;
-
-  // 2) Destructuring assignment를 활용한 방법
-  // arr이 reference type이라 가능
   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-
-  // 3) XOR 연산을 활용한 방법
-  // arr이 reference type이라 가능
-  // arr[idx1] ^= arr[idx2];
-  // arr[idx2] ^= arr[idx1];
-  // arr[idx1] ^= arr[idx2];
 }
 
 function getParentIdx(idx) {
@@ -44,13 +29,41 @@ function getParentIdx(idx) {
 function insert(heap, item) {
   // TODO: 여기에 코드를 작성합니다.
   heap.push(item);
-  let curIdx = heap.length - 1;
-  let pIdx = getParentIdx(curIdx);
-  while (pIdx >= 0 && heap[curIdx] > heap[pIdx]) {
-    swap(curIdx, pIdx, heap);
-    curIdx = pIdx;
-    pIdx = getParentIdx(curIdx);
+  if (heap.length > 1) {
+    let curIdx = heap.length - 1;
+    let pIdx = getParentIdx(curIdx);
+    while (pIdx >= 0 && heap[curIdx] < heap[pIdx]) {
+      swap(curIdx, pIdx, heap);
+      curIdx = pIdx;
+      pIdx = getParentIdx(curIdx);
+    }
   }
+  return heap;
+}
+
+function removeRoot(heap) {
+  // TODO: 여기에 코드를 작성합니다.
+  swap(0, heap.length - 1, heap);
+  heap.pop();
+  if (heap.length === 0) return [];
+
+  let curIdx;
+  let minIdx = 0;
+  while (curIdx !== minIdx) {
+    curIdx = minIdx;
+    let left = curIdx * 2 + 1;
+    let right = curIdx * 2 + 2;
+    if (left < heap.length && heap[left] < heap[minIdx]) {
+      minIdx = left;
+    }
+
+    if (right < heap.length && heap[right] < heap[minIdx]) {
+      minIdx = right;
+    }
+
+    swap(curIdx, minIdx, heap);
+  }
+
   return heap;
 }
 
@@ -59,4 +72,15 @@ const binaryHeap = function (arr) {
   return arr.reduce((heap, item) => {
     return insert(heap, item);
   }, []);
+};
+
+const heapSort = function (arr) {
+  let minHeap = binaryHeap(arr);
+  // TODO: 여기에 코드를 작성합니다.
+  const sorted = [];
+  while (minHeap.length > 0) {
+    sorted.push(minHeap[0]);
+    minHeap = removeRoot(minHeap);
+  }
+  return sorted;
 };
