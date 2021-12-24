a2da21b61a5700f08572dca4047d261fac9f8e60
Hojin Jang <onewithtruth@gmail.com>
2021-12-24 15:30:46 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -17,70 +17,115 @@ rl.on("line", function (line) {
 });
 
 
-function swap(idx1, idx2, arr) {
-  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-}
+// naive solution: O(N) (search only)
+// const rangeMinimum = function (arr, ranges) {
+//   return ranges.map((range) => {
+//     const [start, end] = range;
+//     let min = Number.MAX_SAFE_INTEGER;
+//     for (let i = start; i <= end; i++) {
+//       if (arr[i] < min) min = arr[i];
+//     }
+//     return min;
+//   });
+// };
 
-function getParentIdx(idx) {
-  // TODO: 여기에 코드를 작성합니다.
-  return Math.floor((idx - 1) / 2);
-}
-
-function insert(heap, item) {
-  // TODO: 여기에 코드를 작성합니다.
-  heap.push(item);
-  if (heap.length > 1) {
-    let curIdx = heap.length - 1;
-    let pIdx = getParentIdx(curIdx);
-    while (pIdx >= 0 && heap[curIdx] < heap[pIdx]) {
-      swap(curIdx, pIdx, heap);
-      curIdx = pIdx;
-      pIdx = getParentIdx(curIdx);
-    }
-  }
-  return heap;
-}
-
-function removeRoot(heap) {
-  // TODO: 여기에 코드를 작성합니다.
-  swap(0, heap.length - 1, heap);
-  heap.pop();
-  if (heap.length === 0) return [];
-
-  let curIdx;
-  let minIdx = 0;
-  while (curIdx !== minIdx) {
-    curIdx = minIdx;
-    let left = curIdx * 2 + 1;
-    let right = curIdx * 2 + 2;
-    if (left < heap.length && heap[left] < heap[minIdx]) {
-      minIdx = left;
+// solution with segment tree: O(logN) (search only)
+// object implementaion
+const rangeMinimum = function (arr, ranges) {
+  // ts: tree start. te: tree end
+  // arr의 ts부터 te까지를 tree로 만든다.
+  const createMinTree = (arr, ts, te) => {
+    // base case
+    if (ts === te) {
+      return { value: arr[ts] };
     }
 
-    if (right < heap.length && heap[right] < heap[minIdx]) {
-      minIdx = right;
+    // recursive case
+    // 현재 범위를 절반을 기준으로 왼쪽과 오른쪽으로 나눈다
+    const mid = parseInt((ts + te) / 2);
+    const left = createMinTree(arr, ts, mid);
+    const right = createMinTree(arr, mid + 1, te);
+
+    return {
+      value: Math.min(left.value, right.value),
+      left,
+      right,
+    };
+  };
+  const tree = createMinTree(arr, 0, arr.length - 1);
+
+  // rs: range start, re: reange end
+  const findMin = (ts, te, rs, re, tree) => {
+    // 현재 tree와 구간이 정확히 일치하거나
+    // 구간이 tree를 포함할 경우
+    if (rs <= ts && te <= re) {
+      return tree.value;
     }
 
-    swap(curIdx, minIdx, heap);
-  }
+    // 현재 tree에 주어진 구간이 겹치지 않는 경우
+    if (te < rs || re < ts) {
+      return Number.MAX_SAFE_INTEGER;
+    }
 
-  return heap;
-}
+    // 겹치는 부분이 존재하는 경우
+    const mid = parseInt((ts + te) / 2);
+    return Math.min(
+      findMin(ts, mid, rs, re, tree.left), //
+      findMin(mid + 1, te, rs, re, tree.right)
+    );
+  };
 
-// 아래 코드는 수정하지 마세요.
-const binaryHeap = function (arr) {
-  return arr.reduce((heap, item) => {
-    return insert(heap, item);
-  }, []);
+  const mins = ranges.map((range) => {
+    const [start, end] = range;
+    return findMin(0, arr.length - 1, start, end, tree);
+  });
+  return mins;
 };
 
-const heapSort = function (arr) {
-  let minHeap = binaryHeap(arr);
-  // TODO: 여기에 코드를 작성합니다.
-  const sorted = [];
-  while (minHeap.length > 0) {
-    sorted.push(minHeap[0]);
-    minHeap = removeRoot(minHeap);
-  }
-  return sorted;
-};
+// solution with segment tree: O(logN) (search only)
+// array implementaion
+// const rangeMinimum = function (arr, ranges) {
+//   const createMinTree = (arr, ts, te, tree, idx) => {
+//     if (ts === te) {
+//       tree[idx] = arr[ts];
+//       return arr[ts];
+//     }
+
+//     const mid = Math.floor((ts + te) / 2);
+//     tree[idx] = Math.min(
+//       createMinTree(arr, ts, mid, tree, idx * 2 + 1), //
+//       createMinTree(arr, mid + 1, te, tree, idx * 2 + 2)
+//     );
+
+//     return tree[idx];
+//   };
+
+//   // 트리 전체의 높이(루트 노트에서 가장 깊은 리프 노드까지의 거리)를 구하고
+//   // 전체 배열의 크기를 구한다.
+//   const height = Math.ceil(Math.log2(arr.length));
+//   const size = Math.pow(2, height + 1) - 1;
+//   const tree = Array(size).fill(null);
+//   createMinTree(arr, 0, arr.length - 1, tree, 0);
+
+//   const findMin = (ts, te, rs, re, idx) => {
+//     if (rs <= ts && te <= re) {
+//       return tree[idx];
+//     }
+
+//     if (te < rs || re < ts) {
+//       return Number.MAX_SAFE_INTEGER;
+//     }
+
+//     const mid = parseInt((ts + te) / 2);
+//     return Math.min(
+//       findMin(ts, mid, rs, re, 2 * idx + 1), //
+//       findMin(mid + 1, te, rs, re, 2 * idx + 2)
+//     );
+//   };
+
+//   const mins = ranges.map((range) => {
+//     const [start, end] = range;
+//     return findMin(0, arr.length - 1, start, end, 0);
+//   });
+//   return mins;
+// };
