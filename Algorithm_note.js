8bf37dfcdfd184b779154b09f0bd2cfa85e34e35
Hojin Jang <onewithtruth@gmail.com>
2021-12-25 19:44:44 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -1,131 +1,88 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-let input = [];
+// let input = [];
 
-rl.on("line", function (line) {
-  input.push(Number(line));
-  // input = line.split(" ").map((el) => Number(el));
-  // input.push(line.split(" ").map((el) => Number(el)));
-}).on("close", function () {
+// rl.on("line", function (line) {
+//   input.push(Number(line));
+//   // input = line.split(" ").map((el) => Number(el));
+//   // input.push(line.split(" ").map((el) => Number(el)));
+// }).on("close", function () {
   
 
-  process.exit();
-});
+//   process.exit();
+// });
 
+////////////////////////////////////
 
-// naive solution: O(N) (search only)
-// const rangeMinimum = function (arr, ranges) {
-//   return ranges.map((range) => {
-//     const [start, end] = range;
-//     let min = Number.MAX_SAFE_INTEGER;
-//     for (let i = start; i <= end; i++) {
-//       if (arr[i] < min) min = arr[i];
+// naive solution: O(N^2)
+// const largestRectangularArea = function (histogram) {
+//   let largest = 0;
+//   // 모든 연속된 부분 히스토그램을 고려한다.
+//   // 밑변의 길이를 부분 히스토그램의 길이로 고정하면, 높이는 가장 낮은 막대의 높이가 된다.
+//   for (let left = 0; left < histogram.length; left++) {
+//     // 길이가 1인 막대로 만들 수 있는 직사각형의 넓이는 막대의 높이와 같다.
+//     let min = histogram[left];
+//     for (let right = left; right < histogram.length; right++) {
+//       // left부터 right까지의 히스토그램의 막대 중 가장 낮은 막대의 높이를 구한다.
+//       if (histogram[right] < min) min = histogram[right];
+//       // 해당 구간(left ~ right)의 막대를 전부 포함해서 만들 수 있는 직사각형의 넓이를 구한다.
+//       let area = min * (right - left + 1);
+//       // 매번 구한 면적을 기존의 면적과 비교해 갱신한다.
+//       if (area > largest) largest = area;
 //     }
-//     return min;
-//   });
+//   }
+//   return largest;
 // };
 
-// solution with segment tree: O(logN) (search only)
-// object implementaion
-const rangeMinimum = function (arr, ranges) {
-  // ts: tree start. te: tree end
-  // arr의 ts부터 te까지를 tree로 만든다.
-  const createMinTree = (arr, ts, te) => {
-    // base case
-    if (ts === te) {
-      return { value: arr[ts] };
-    }
+// divide and conquer: O(N * logN)
+const largestRectangularArea = function (histogram) {
+  const createMinIdxTree = (arr, ts, te) => {
+    // 가장 작은 값의 '인덱스'를 구하기 위한 구간 트리
+    if (ts === te) return { idx: ts, val: arr[ts] };
 
-    // recursive case
-    // 현재 범위를 절반을 기준으로 왼쪽과 오른쪽으로 나눈다
     const mid = parseInt((ts + te) / 2);
-    const left = createMinTree(arr, ts, mid);
-    const right = createMinTree(arr, mid + 1, te);
+    const left = createMinIdxTree(arr, ts, mid);
+    const right = createMinIdxTree(arr, mid + 1, te);
 
     return {
-      value: Math.min(left.value, right.value),
+      val: Math.min(left.val, right.val),
+      idx: left.val < right.val ? left.idx : right.idx,
       left,
       right,
     };
   };
-  const tree = createMinTree(arr, 0, arr.length - 1);
+  const tree = createMinIdxTree(histogram, 0, histogram.length - 1);
 
-  // rs: range start, re: reange end
-  const findMin = (ts, te, rs, re, tree) => {
-    // 현재 tree와 구간이 정확히 일치하거나
-    // 구간이 tree를 포함할 경우
-    if (rs <= ts && te <= re) {
-      return tree.value;
-    }
+  const getMinIdx = (ts, te, rs, re, tree) => {
+    if (rs <= ts && te <= re) return tree.idx;
+    if (te < rs || re < ts) return rs;
 
-    // 현재 tree에 주어진 구간이 겹치지 않는 경우
-    if (te < rs || re < ts) {
-      return Number.MAX_SAFE_INTEGER;
-    }
-
-    // 겹치는 부분이 존재하는 경우
     const mid = parseInt((ts + te) / 2);
-    return Math.min(
-      findMin(ts, mid, rs, re, tree.left), //
-      findMin(mid + 1, te, rs, re, tree.right)
+    const left = getMinIdx(ts, mid, rs, re, tree.left);
+    const right = getMinIdx(mid + 1, te, rs, re, tree.right);
+    return histogram[left] < histogram[right] ? left : right;
+  };
+
+  const getRangeArea = (start, end) => {
+    if (start > end) return 0;
+    // 현재 구간에서 가장 작은 막대를 찾는다.
+    // 가장 작은 막대이므로 구간의 길이 * 높이만큼의 직사각형을 만들 수 있다. (첫번째 후보)
+    const minIdx = getMinIdx(0, histogram.length - 1, start, end, tree);
+
+    // 가장 작은 막대를 기준으로 왼쪽, 오른쪽 부분에 존재하는 모든 막대의 높이가 더 크다.
+    // 재귀적으로 왼쪽 부분과 오른쪽 부분,
+    // 즉 해당 구간에서 가장 작은 막대를 제외해서 만들 수 있는 가장 큰 직사각형의 넓이를 구한다.
+    return Math.max(
+      (end - start + 1) * histogram[minIdx], // 첫번째 후보
+      getRangeArea(start, minIdx - 1),
+      getRangeArea(minIdx + 1, end)
     );
   };
 
-  const mins = ranges.map((range) => {
-    const [start, end] = range;
-    return findMin(0, arr.length - 1, start, end, tree);
-  });
-  return mins;
+  return getRangeArea(0, histogram.length - 1);
 };
 
-// solution with segment tree: O(logN) (search only)
-// array implementaion
-// const rangeMinimum = function (arr, ranges) {
-//   const createMinTree = (arr, ts, te, tree, idx) => {
-//     if (ts === te) {
-//       tree[idx] = arr[ts];
-//       return arr[ts];
-//     }
-
-//     const mid = Math.floor((ts + te) / 2);
-//     tree[idx] = Math.min(
-//       createMinTree(arr, ts, mid, tree, idx * 2 + 1), //
-//       createMinTree(arr, mid + 1, te, tree, idx * 2 + 2)
-//     );
-
-//     return tree[idx];
-//   };
-
-//   // 트리 전체의 높이(루트 노트에서 가장 깊은 리프 노드까지의 거리)를 구하고
-//   // 전체 배열의 크기를 구한다.
-//   const height = Math.ceil(Math.log2(arr.length));
-//   const size = Math.pow(2, height + 1) - 1;
-//   const tree = Array(size).fill(null);
-//   createMinTree(arr, 0, arr.length - 1, tree, 0);
-
-//   const findMin = (ts, te, rs, re, idx) => {
-//     if (rs <= ts && te <= re) {
-//       return tree[idx];
-//     }
-
-//     if (te < rs || re < ts) {
-//       return Number.MAX_SAFE_INTEGER;
-//     }
-
-//     const mid = parseInt((ts + te) / 2);
-//     return Math.min(
-//       findMin(ts, mid, rs, re, 2 * idx + 1), //
-//       findMin(mid + 1, te, rs, re, 2 * idx + 2)
-//     );
-//   };
-
-//   const mins = ranges.map((range) => {
-//     const [start, end] = range;
-//     return findMin(0, arr.length - 1, start, end, 0);
-//   });
-//   return mins;
-// };
