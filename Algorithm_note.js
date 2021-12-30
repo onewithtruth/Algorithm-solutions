61b9c1a1c06d9d700fd7474a2c92601341b9870e
Hojin Jang <onewithtruth@gmail.com>
2021-12-30 16:21:42 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -18,29 +18,40 @@
 
 ////////////////////////////////////
 
-const LCS = function (str1, str2) {
-  const M = str1.length;
-  const N = str2.length;
-  const memo = [];
-  // 중복 계산을 방지하기 위해 left, right
-  for (let i = 0; i < M + 1; i++) memo.push(Array(N + 1).fill(-1));
+const uglyNumbers = function (n) {
+  // 매번 나눗셈 연산을 하는 것이 비효율적이므로
+  // 이미 구한 수에서부터 구한다.
 
-  const compareOneByOne = (left, right, len) => {
-    if (memo[left][right] !== -1) return memo[left][right];
+  const uglyNumbers = [1];
+  let idx2 = 0,
+    idx3 = 0,
+    idx5 = 0;
 
-    if (left === str1.length || right === str2.length) return 0;
-
-    if (str1[left] === str2[right]) {
-      memo[left][right] = 1 + compareOneByOne(left + 1, right + 1, len + 1);
-      return memo[left][right];
-    }
-
-    memo[left][right] = Math.max(
-      compareOneByOne(left + 1, right, len), //
-      compareOneByOne(left, right + 1, len)
+  for (let i = 0; i < n; i++) {
+    // 1. 가장 작은 수인 1에 2, 3, 5를 곱한 수 중에 가장 작은 수를 구한다.
+    // 2. 2가 선택됨.
+    // 3. 2는 가장 작은 수 1에 곱해졌으므로
+    //   3.1 이제 2는 그 다음 작은 수인 2에 곱해지고
+    //   3.2 3, 5는 여전히 가장 작은 수에 곱해진다.
+    // 4. 3에서 가장 작은 수는 3. 3은 이제 다음으로 작은 수인 2에 곱혀진다.
+    // 5. 반복
+    const nextMultipleOf2 = uglyNumbers[idx2] * 2;
+    const nextMultipleOf3 = uglyNumbers[idx3] * 3;
+    const nextMultipleOf5 = uglyNumbers[idx5] * 5;
+    const nextUglyNum = Math.min(
+      nextMultipleOf2,
+      nextMultipleOf3,
+      nextMultipleOf5
     );
-    return memo[left][right];
-  };
+    uglyNumbers.push(nextUglyNum);
 
-  return compareOneByOne(0, 0, 0);
+    // 같은 수를 중복해서 저장할 수 있으므로,
+    // 각각 별도의 조건문으로 작성해야 한다.
+    //  2 * 3 = 6
+    //  3 * 2 = 6
+    if (nextUglyNum === nextMultipleOf2) idx2++;
+    if (nextUglyNum === nextMultipleOf3) idx3++;
+    if (nextUglyNum === nextMultipleOf5) idx5++;
+  }
+  return uglyNumbers[n - 1];
 };
