a35c7bb14db01cfe13f4ed8449467f1cc11b5ca3
Hojin Jang <onewithtruth@gmail.com>
2021-12-31 21:51:23 +0900

    Update Algorithm_note.js

--- Algorithm_note.js
+++ Algorithm_note.js
@@ -18,136 +18,80 @@
 
 ////////////////////////////////////
 
-// 좌표평면 위의 두 점 사이의 거리를 계산하는 함수입니다.
-function calculateDistance(p1, p2) {
-  const yDiffSquared = Math.pow(p2[0] - p1[0], 2);
-  const xDiffSquared = Math.pow(p2[1] - p1[1], 2);
-  const dist = Math.sqrt(yDiffSquared + xDiffSquared);
-  return Math.round(dist * 100);
-}
+// naive solution: O(2^M * N))
+// 6을 만드는 방법 중 [1, 5]와 [5, 1]을 중복해서 세면 안 되기 때문에.
+// 동전을 순서대로 사용한다.
+// const coinChange = function (total, coins) {
+//   const makeChageFrom = (left, idx) => {
+//     if (left === 0) return 1;
 
-// naive solution: O(N^2)
-// 모든 쌍을 비교하는 방법
-// const closestPairOfPoints = function (points) {
-//   let min = Number.MAX_SAFE_INTEGER;
-//   for (let src = 0; src < points.length; src++) {
-//     for (let dst = src + 1; dst < points.length; dst++) {
-//       const dist = calculateDistance(points[src], points[dst]);
-//       min = Math.min(min, dist);
+//     let cnt = 0;
+//     // 지금 사용하고 있는 동전부터만 고려한다.
+//     for (let i = idx; i < coins.length; i++) {
+//       if (left - coins[i] >= 0) {
+//         cnt = cnt + makeChageFrom(left - coins[i], i);
+//       }
 //     }
-//   }
-//   return min;
+
+//     return cnt;
+//   };
+//   // 0번째 동전부터 사용한다.
+//   return makeChageFrom(total, 0);
 // };
 
-const merge = function (left, right, comparator = (item) => item) {
-  let merged = [];
-  let leftIdx = 0,
-    rightIdx = 0;
-  const size = left.length + right.length;
+// simpler recursion
+// and dynamic programming with memoization: O(M * N)
+const coinChange = function (total, coins) {
+  // memo[i][j]는 i만큼의 금액을 coins[j]부터 ~ coins[coins.length - 1]까지 사용하여 만들 수 있는 경우의 수를 저장
+  const memo = [];
+  for (let i = 0; i < total + 1; i++) memo.push(Array(coins.length).fill(-1));
+  const makeChageFrom = (left, idx) => {
+    // 0을 만드는 방법은 1가지이다. 아니면 목표 금액을 만들었다고 생각해도 된다.
+    if (left === 0) return 1;
+    // 금액이 마이너스가 되는 경우는 불가능하므로 0을 리턴
+    if (left < 0) return 0;
+    // 동전을 전부 검토해서, 남아있는 새로운 동전은 없는데 목표 금액을 만들지 못한 경우 (실패)
+    if (idx >= coins.length && left > 0) return 0;
+    // 이미 해결한 적이 있는 문제는 다시 풀지 않는다.
+    if (memo[left][idx] !== -1) return memo[left][idx];
 
-  for (let i = 0; i < size; i++) {
-    if (leftIdx >= left.length) {
-      merged.push(right[rightIdx]);
-      rightIdx++;
-    } else if (
-      rightIdx >= right.length ||
-      comparator(left[leftIdx]) <= comparator(right[rightIdx])
-    ) {
-      merged.push(left[leftIdx]);
-      leftIdx++;
-    } else {
-      merged.push(right[rightIdx]);
-      rightIdx++;
-    }
-  }
+    // left만큼의 금액을 coins[idx]부터 사용하여 만들 수 있는 경우의 수는
+    //  1) coins[idx]는 그만 사용하고, 다음 동전으로 넘어가거나 (목표 금액은 그대로이고, idx가 증가한다.)
+    //  2)) coins[idx]를 한번 더 사용한다. coins[idx]를 또 사용할 수 있으므로, idx는 그대로이고, 목표 금액은 coins[i]만큼 줄어든다.
+    memo[left][idx] =
+      makeChageFrom(left, idx + 1) + makeChageFrom(left - coins[idx], idx);
+    return memo[left][idx];
+  };
 
-  return merged;
+  return makeChageFrom(total, 0);
 };
 
-const mergeSort = function (arr, comparator) {
-  const aux = (start, end) => {
-    if (start >= end) return [arr[start]];
-    const mid = Math.floor((start + end) / 2);
-    const left = aux(start, mid);
-    const right = aux(mid + 1, end);
-    return merge(left, right, comparator);
-  };
-  return aux(0, arr.length - 1);
-};
+// dynamic programming with tabulation: O(M * N)
+// const coinChange = function (total, coins) {
+//   // table[i][j]는 coins[j]까지 사용해서 i만큼의 금액을 만들 수 있는 경우의 수를 저장
+//   const table = [];
+//   for (let i = 0; i < total + 1; i++) table.push(Array(coins.length).fill(0));
+//   // 모든 경우에 0을 만들 수 있는 경우는 1 (base case)
+//   for (let i = 0; i < coins.length; i++) table[0][i] = 1;
 
-// divide and conquer: O(N * logN)
-const closestPairOfPoints = function (points) {
-  const bruteForce = (start, end, sorted) => {
-    // naive solution과 동일한 로직
-    // 모든 쌍을 비교한다. 3개 이하에 대해서만 호출되므로 크게 비효율적이지 않다.
-    let min = Number.MAX_SAFE_INTEGER;
-    for (let src = start; src <= end; src++) {
-      for (let dst = src + 1; dst <= end; dst++) {
-        const dist = calculateDistance(sorted[src], sorted[dst]);
-        min = Math.min(min, dist);
-      }
-    }
-    return min;
-  };
+//   for (let amount = 1; amount <= total; amount++) {
+//     // 작은 금액부터 차례대로 경우의 수를 구한다. (bottom-up)
+//     for (let idx = 0; idx < coins.length; idx++) {
+//       let coinIncluded = 0;
+//       if (amount - coins[idx] >= 0) {
+//         coinIncluded = table[amount - coins[idx]][idx];
+//       }
 
-  const closestCrossing = (mid, sorted, min) => {
-    // 가운데(mid)를 기준으로
-    const strip = [];
-    const midX = sorted[mid][1];
-    let lIdx = mid - 1;
-    let rIdx = mid + 1;
+//       let coinExcluded = 0;
+//       if (idx >= 1) {
+//         // 동전을 순서대로 검사하고 있기 때문에, 바로 직전의 경우만 고려하면 된다.
+//         // 단, 0번째 동전은 직전이 없으므로 제외한다.
+//         coinExcluded = table[amount][idx - 1];
+//       }
 
-    // 왼쪽과 오른쪽 부분에서 가장 가까운 두 점 사이의 거리가 min으로 주어진다.
-    // 가운데를 기준으로 오직 x좌표만을 기준으로 min보다 가까운 좌표만 고려한다.
-    // y좌표가 같다고 가정하면, 최소한 이 조건(x좌표 기준 min 이하)을 만족해야하기 때문이다.
-    // y좌표가 같을 경우 두 점 사이의 거리는 x축 좌표 간의 거리다.
-    // 단, 소수점 계산을 피하기 위해 두 점 사이의 거리에 100을 곱하고 있으므로 동일한 기준을 적용해야 한다.
+//       table[amount][idx] = coinIncluded + coinExcluded;
+//     }
+//   }
 
-    // sorted는 x축을 기준으로 정렬되어 있기 때문에,
-    // mid를 기준으로 가까운 거리부터 최소 기준(min보다는 가까워야 함)을 만족할 때까지만 탐색을 하면 된다.
-    while (
-      rIdx < sorted.length &&
-      Math.abs(midX - sorted[rIdx][1]) * 100 < min
-    ) {
-      rIdx++;
-    }
-    while (lIdx >= 0 && Math.abs(midX - sorted[lIdx][1]) * 100 < min) {
-      lIdx--;
-    }
-
-    // while 탈출하기 위한 조건을 보면,
-    // lIdx는 1을 더해야 하고, rIdx는 1을 줄여야 한다.
-    // 아래 구간에 대해서 brute force를 적용한다.
-    for (let i = lIdx + 1; i < rIdx; i++) {
-      for (let j = i + 1; j < rIdx; j++) {
-        min = Math.min(min, calculateDistance(sorted[i], sorted[j]));
-      }
-    }
-    return min;
-  };
-
-  const closestFrom = (start, end, size, sorted) => {
-    if (size <= 3) {
-      // 최소 두 개 이상의 점이 있어야 거리를 계산할 수 있다.
-      //  1) 모든 점의 개수가 4개일 경우, 각각 2개로 나눌 수 있다.
-      //  2) 모든 점의 개수가 5개일 경우, 각각 2, 3개로 나눌 수 있다. 3개인 경우 더 나눌 수 없다.
-      return bruteForce(start, end, sorted);
-    }
-    // 가운데를 기준으로 분할한 뒤 재귀적으로 문제를 해결한다.
-    const mid = Math.floor((start + end) / 2);
-    // 왼쪽, 오른쪽으로 나뉜 부분에서 각각 가장 가까운 두 점 사이의 거리를 구한다.
-    const minLeft = closestFrom(start, mid, mid - start + 1, sorted);
-    const minRight = closestFrom(mid + 1, end, end - mid, sorted);
-
-    // 전체 영역에서 가장 가까운 두 점 사이의 거리는 아래 중 하나다.
-    //  1) 위에서 구한 두 거리(minLeft, minRight)
-    //  2) 가운데를 가로지르는 두 점 사이의 거리
-    // 먼저 1)중에서 더 짧은 거리를 구한다. 최종 정답은 이보다 작거나 같아야 한다.
-    let min = Math.min(minLeft, minRight);
-    return closestCrossing(mid, sorted, min);
-  };
-
-  // x좌표를 기준으로 정렬한다.
-  const sorted = mergeSort(points.slice(0), (item) => item[1]);
-  return closestFrom(0, sorted.length - 1, sorted.length, sorted);
-};
+//   return table[total][coins.length - 1];
+// };
